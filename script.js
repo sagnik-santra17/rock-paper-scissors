370c50ed11916e5aac388379e1d9ca4aa185a8cd
Sagnik Santra <sagnik.santra17@gmail.com>
2025-07-22 13:26:51 +0530

    Final changes before merge

--- script.js
+++ script.js
@@ -1,54 +1,105 @@
 const playGame = function () {
   let humanScore = 0;
   let computerScore = 0;
+  let gameOver = false;
 
-  // while (humanScore < 5 && computerScore < 5) {
+  const rockBtn = document.querySelector(".rock");
+  const paperBtn = document.querySelector(".paper");
+  const scissorsBtn = document.querySelector(".scissors");
+
+  rockBtn.addEventListener("click", () => handleClick("rock"));
+  paperBtn.addEventListener("click", () => handleClick("paper"));
+  scissorsBtn.addEventListener("click", () => handleClick("scissors"));
+
   const getComputerChoice = () => {
     const picks = ["rock", "paper", "scissors"];
     const randomPick = Math.floor(Math.random() * picks.length);
     return picks[randomPick];
   };
 
-  const getHumanChoice = window.prompt("Choose your pick");
-  const computerChoice = getComputerChoice();
-  const humanChoice = getHumanChoice.toLowerCase();
+  const selections = document.querySelector(".selections");
+  const playerSelection = document.createElement("h3");
+  const computerSelection = document.createElement("h3");
+  selections.append(playerSelection, computerSelection);
 
-  console.log(computerChoice);
-  console.log(humanChoice);
+  const scores = document.querySelector(".scores");
+  const compScore = document.createElement("h3");
+  const playerScore = document.createElement("h3");
+  scores.append(playerScore, compScore);
+
+  function handleClick(playerChoice) {
+    if (gameOver) return;
+
+    const computerChoice = getComputerChoice();
+
+    //this outputs the selections in DOM
+
+    playerSelection.textContent = `Player chose => ${playerChoice}`;
+    computerSelection.textContent = `Computer chose => ${computerChoice}`;
+
+    //------ outputs in DOM
+
+    playRound(playerChoice, computerChoice);
+  }
+
+  const draw = document.querySelector(".draw");
+  const playerWins = document.querySelector(".playerWins");
+  const computerWins = document.querySelector(".computerWins");
+  const errorMsg = document.querySelector(".errorMsg");
+  const pick = document.querySelector(".pick");
+
+  const ifCompWins = document.querySelector(".finalResultsComp");
+  ifCompWins.style.display = "none";
+
+  const ifPlayerWins = document.querySelector(".finalResultsPlayer");
+  ifPlayerWins.style.display = "none";
 
   const playRound = function (player, computer) {
+    draw.style.display = "none";
+    playerWins.style.display = "none";
+    computerWins.style.display = "none";
+    errorMsg.style.display = "none";
+    pick.style.display = "none";
+
     if (player === computer) {
-      console.log("It's a draw!");
+      draw.style.display = "block";
     } else if (
       (player === "rock" && computer === "scissors") ||
       (player === "paper" && computer === "rock") ||
       (player === "scissors" && computer === "paper")
     ) {
-      console.log("You win this round!");
+      playerWins.style.display = "block";
       humanScore++;
     } else if (
       (computer === "rock" && player === "scissors") ||
       (computer === "paper" && player === "rock") ||
       (computer === "scissors" && player === "paper")
     ) {
-      console.log("Computer wins this round!");
+      computerWins.style.display = "block";
       computerScore++;
     } else {
-      console.log("Invalid input. Please choose rock, paper, or scissors.");
+      errorMsg.style.display = "block";
     }
 
-    console.log(humanScore);
-    console.log(computerScore);
+    //this outputs the scores
+
+    compScore.textContent = `Computer score: ${computerScore}`;
+
+    playerScore.textContent = `Player score: ${humanScore}`;
+
+    if (computerScore === 5) {
+      ifCompWins.style.display = "block";
+      gameOver = true;
+    } else if (humanScore === 5) {
+      ifPlayerWins.style.display = "block";
+      gameOver = true;
+    }
   };
-  playRound(humanChoice, computerChoice);
 };
 
-if (computerScore === 5) {
-  console.log("Computer won the game");
-} else if (humanScore === 5) {
-  console.log("Player won the game");
-}
-// };
-``;
-
 playGame();
+
+const resetGame = document.querySelector(".reset");
+resetGame.addEventListener("click", () => {
+  location.reload();
+});
